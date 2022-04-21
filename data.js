ed6012f0f5df1eb4b0f3c47475fc9dfda7163cb4
aririggins <66768127+aririggins@users.noreply.github.com>
2022-04-21 03:59:27 -0400

    New updates w Prelem Data

---
+++ data.js
@@ -0,0 +1,21 @@
+// the data
+var nameArray = [];
+nameArray[0] = "";
+nameArray[1] = "Black";
+nameArray[2] = "White";
+nameArray[3] = "Asian";
+nameArray[4] = "Mixed";
+
+var catArray = [];
+catArray[0] = "% of Top 50"
+catArray[1] = "Ratio of Women"
+catArray[2] = "Ratio of Men"
+catArray[3] = "% of US"
+
+var dataArray = [];
+dataArray[0] = ['Top_50', '6', '60', '18', '16'];
+dataArray[1] = ['Women_50', '0', '26', '6', '6'];
+dataArray[2] = ['Men_50', '6', '34', '12', '10'];
+dataArray[3] = ['US', '13.4', '76.3', '5.9', '2.8'];
+// console.log(dataArray);
+// console.log(dataArray[1][2]);
---
+++ data.py
@@ -0,0 +1,111 @@
+import csv
+
+filename = "data.csv"
+
+fields = []
+rows = []
+
+# Read the file
+with open(filename, 'r') as csvfile:
+    # creating a csv reader object
+    csvreader = csv.reader(csvfile)
+     
+    # extracting field names through first row
+    fields = next(csvreader)
+ 
+    # extracting each data row one by one
+    for row in csvreader:
+        rows.append(row)
+
+    # get total number of rows
+    # print("Total no. of rows: %d"%(csvreader.line_num))
+
+# printing the field names
+# print('Field names are:' + ', '.join(field for field in fields))
+ 
+#  printing first 5 rows
+# print('\nFirst 5 rows are:\n')
+# for row in rows[:5]:
+#     # parsing each column of a row
+#     for col in row:
+#         print("%10s"%col,end=" "),
+#     print('\n')
+
+jsCode = "var dataArray = [];"
+jsCode += '\n'
+jsCode += "var nameArray = [];"
+jsCode += '\n'
+# generate javascript code from this
+count = 0
+for field in fields:
+    jsCode += "nameArray[" + str(count) + "] = " + field + ";"
+    # jsCode += "var field" + str(count) + " = " + field + ";"
+    jsCode += '\n'
+    count = count + 1
+
+# count = 1
+# for row in rows:
+#     jsCode += "dataArray[0][" + str(count) + "] = " + row[0] + ";"
+#     count = count + 1
+#     jsCode += '\n'
+    # jsCode += "var name" + str(count) + " = " + row[0] + ";"
+
+#Actually just make an array instead and parse this in the javascript lol
+
+count = 0
+# countCol = 0
+# for row in rows:
+#     # parsing each column of a row
+#     for col in row:
+#         info = "info = const " + str(count) + " = {" + rows[count][0] + ":" + col + ";"
+#         jsCode += info
+#         jsCode += '\n'
+#         # jsCode += "dataArray[" + str(count) + "] = dataArray[" + str(count) + "] +"
+#         jsCode += "dataArray[" + str(count) + "] = info;"
+#         jsCode += '\n'
+#         countCol += 1
+#     count += 1
+
+
+# for row in rows:
+#     colCount = 0
+#     # info = "dataArray[" + str(count) + "] = {"
+#     for col in row:
+#         # jsCode += "info = "
+#         jsCode += "dataArray[" + str(count) + "] = " + col + ";"
+#         count += 1
+#         jsCode += '\n'
+# print(jsCode)
+
+# jsCode += '\n'
+# for field in fields:
+#     for row in fields:
+#         info = "info = const " + str(count) + " = {" + field + ":" + row + ";"
+#         jsCode += info
+#         jsCode += '\n'
+#         # jsCode += "dataArray[" + str(count) + "] = dataArray[" + str(count) + "] +"
+#         jsCode += "dataArray[" + str(count) + "] = info;"
+#         jsCode += '\n'
+
+# print(jsCode)
+
+#this kinda works!
+for row in rows:
+    info = "dataArray[" +str(count) + "] = " + str(row) + ";"
+    jsCode += info
+    jsCode += '\n'
+    count += 1
+
+print(jsCode)
+
+# for row in rows:
+#     count = 0
+#     for col in row:
+#         info = "info = const " + str(count) + " = {" + rows[count][0] + ":"
+#         for row in row:
+#             info += col + ", "
+#         count += 1
+#     info += "}"
+#     print(info)
+
+
