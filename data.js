c2850c7119ce694bd0937b2b86658b382afb5889
aririggins <66768127+aririggins@users.noreply.github.com>
2022-04-26 06:35:31 -0400

    final data

--- data.js
+++ data.js
@@ -5,17 +5,20 @@ nameArray[1] = "Black";
 nameArray[2] = "White";
 nameArray[3] = "Asian";
 nameArray[4] = "Mixed";
+nameArray[5] = "Latinx";
 
 var catArray = [];
-catArray[0] = "% of Top 50"
-catArray[1] = "Ratio of Women"
-catArray[2] = "Ratio of Men"
-catArray[3] = "% of US"
+catArray[0] = 'Top_50Comp'
+catArray[1] = 'Women_50'
+catArray[2] = 'avg_Follows'
+catArray[3] = 'avg_Comments'
+catArray[4] = 'Comment_Rating'
 
 var dataArray = [];
-dataArray[0] = ['Top_50', '6', '60', '18', '16'];
-dataArray[1] = ['Women_50', '0', '26', '6', '6'];
-dataArray[2] = ['Men_50', '6', '34', '12', '10'];
-dataArray[3] = ['US', '13.4', '76.3', '5.9', '2.8'];
+dataArray[0] = ['Top_50Comp', '44.78', '78.63', '305.1', '571.43', '97.3'];
+dataArray[1] = ['Women_50', '0', '43.33', '33.33', '37.5', '55.56'];
+dataArray[2] = ['avg_Follows', '86.43', '95.96', '64.23', '57.16', '56.00'];
+dataArray[3] = ['avg_Comments', '22.59', '27.57', '97.77', '18.97', '14.92'];
+dataArray[4] = ['Comment_Rating', '73.65', '94.1', '66.5', '88.7', '63.2'];
 // console.log(dataArray);
 // console.log(dataArray[1][2]);
